20ceecb78a50ad943384c0c109b358b62fb81a84
laurenyip <laurenyip10@gmail.com>
2024-09-05 08:22:57 -0700

    links

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css'; // Add your global styles
+import './index.css'; // Global styles
 import Home from './pages/Home'; // Import the Home component
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Use Routes and Route instead of Switch
+import About from './pages/About'; // Import the About component
+import Projects from './pages/Projects'; // Import the Projects component
+import Portfolio from './pages/Portfolio'; // Import the Portfolio component
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Use Routes and Route
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />  {/* Use element to render the component */}
-        {/* Add other routes here */}
+        <Route path="/" element={<Home />} />         {/* Home route */}
+        <Route path="/about" element={<About />} />   {/* About route */}
+        <Route path="/projects" element={<Projects />} /> {/* Projects route */}
+        <Route path="/portfolio" element={<Portfolio />} /> {/* Portfolio route */}
       </Routes>
     </Router>
   </React.StrictMode>,
