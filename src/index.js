3764185583844944024560a5d36b5d00555cb992
laurenyip <laurenyip10@gmail.com>
2024-09-05 09:32:20 -0700

    meoeo

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 // src/index.js
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // Global styles
-import Home from './pages/Home'; // Import the Home component
-import About from './pages/About'; // Import the About component
-import Projects from './pages/Projects'; // Import the Projects component
-import Portfolio from './pages/Portfolio'; // Import the Portfolio component
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Use Routes and Route
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"; // Global styles
+import Home from "./pages/Home"; // Import the Home component
+import About from "./pages/About"; // Import the About component
+import Projects from "./pages/Projects"; // Import the Projects component
+import Portfolio from "./pages/Portfolio"; // Import the Portfolio component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Use Routes and Route
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />         {/* Home route */}
-        <Route path="/about" element={<About />} />   {/* About route */}
+        <Route path="/" element={<Home />} /> {/* Home route */}
+        <Route path="/about" element={<About />} /> {/* About route */}
         <Route path="/projects" element={<Projects />} /> {/* Projects route */}
-        <Route path="/portfolio" element={<Portfolio />} /> {/* Portfolio route */}
+        <Route path="/portfolio" element={<Portfolio />} />{" "}
+        {/* Portfolio route */}
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
