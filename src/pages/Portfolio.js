ba59707ea50c53cc6cc588d5260f4600f0d6494b
laurenyip <laurenyip10@gmail.com>
2026-01-07 13:16:41 -0800

    img

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -95,7 +95,7 @@ function Portfolio() {
             rel="noopener noreferrer"
             className="rounded-full bg-white border-2 border-gray-700 px-8 py-2 text-sm font-semibold text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 transition-colors"
           >
-            Visit My Substack
+            My writing here → visit my Substack
           </a>
         </div>
 
