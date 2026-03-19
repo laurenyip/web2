8fe516a67599ac776ad858f84713f824b7b5bdcf
laurenyip <laurenyip10@gmail.com>
2026-03-19 11:32:23 -0700

    1108

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,23 +2,25 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import '../pages/App.css'
 
-function Navbar() {
+function Navbar({ className = '' }) {
   return (
-    <ul className="fixed top-8 left-1/2 -translate-x-1/2 w-full max-w-md px-4 md:left-[21%] md:translate-x-0 md:max-w-none md:w-[78%] z-[50] bg-white opacity-70 text-gray-700 font-[Moto] text-lg flex list-none justify-start p-4">
+    <ul
+      className={`fixed top-8 left-1/2 -translate-x-1/2 w-full max-w-md px-4 md:left-[21%] md:translate-x-0 md:max-w-none md:w-[78%] z-[50] bg-white opacity-70 text-gray-700 font-[Moto] text-lg flex list-none justify-start p-4 ${className}`}
+    >
 
       <li className="nav-item">
         <Link className="nav-link" to="/">
-          Home<span className="nav-pipe">&nbsp;|&nbsp;</span>
+          Home<span className="nav-pipe">&nbsp;|</span>
         </Link>
       </li>
       <li className="nav-item">
         <Link className="nav-link" to="/about">
-          About<span className="nav-pipe">&nbsp;|&nbsp;</span>
+          About<span className="nav-pipe">&nbsp;|</span>
         </Link>
       </li>
       <li className="nav-item">
         <Link className="nav-link" to="/projects">
-          Projects<span className="nav-pipe">&nbsp;|&nbsp;</span>
+          Projects<span className="nav-pipe">&nbsp;|</span>
         </Link>
       </li>
       <li className="nav-item">
